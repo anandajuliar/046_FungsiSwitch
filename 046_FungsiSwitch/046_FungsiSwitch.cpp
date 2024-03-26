d0c908ca32dbc434a1d9d11bc0d6261b224a74fb
anandajuliar <anandajulia001@gmail.com>
2024-03-26 17:37:56 +0700

    membuat library global

--- 046_FungsiSwitch/046_FungsiSwitch.cpp
+++ 046_FungsiSwitch/046_FungsiSwitch.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 int bilA, int bilB;
 
+void input()
+{
+    cout << "Masukkan Bilangan Pertama = ";
+    cin >> bilA;
+    cout << "Maukaan Bilanga Kedua = ";
+    cin >> bilB;
+}
+
